2eb95389a1af7172fe38dc8ea18ab9fe5085d5fb
crazyoptimist <hey@crazyoptimist.net>
2024-01-25 03:54:08 -0600

    feat: implemented producer

---
+++ cmd/producer/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"kafka-go-poc/internal/kafka"
+	"kafka-go-poc/pkg/ping"
+)
+
+const (
+	topic             = "topic.v1"
+	kafkaURL          = "localhost:19092"
+	schemaRegistryURL = "http://localhost:8081"
+)
+
+func main() {
+	producer, err := kafka.NewProducer(kafkaURL, schemaRegistryURL)
+	defer producer.Close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	pingMSG := ping.PingMessage{Value: 55}
+	offset, err := producer.ProduceMessage(&pingMSG, topic)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Produced a message successfully. Offset: %v\n", offset)
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module kafka-go-poc
 
 go 1.21.6
 
-require google.golang.org/protobuf v1.32.0
+require (
+	github.com/confluentinc/confluent-kafka-go v1.9.2
+	google.golang.org/protobuf v1.32.0
+)
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/jhump/protoreflect v1.12.0 // indirect
+	google.golang.org/genproto v0.0.0-20220503193339-ba3ae3f07e29 // indirect
+)
